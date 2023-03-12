1221f11918c5e430af3d698cf08e182742340a74
caspereijkens <07.piping.typists@icloud.com>
2023-03-12 10:03:41 +0100

    Packaged objects initiated

--- server/main.go
+++ server/main.go
@@ -1,53 +1,16 @@
 package main
 
 import (
-	"io"
 	"log"
-	"mime"
 	"net/http"
-	"path/filepath"
 	"project/server/config"
+	"project/server/objects"
 	"project/server/posts"
 	"project/server/users"
-
-	"github.com/minio/minio-go"
 )
 
 func styleSheetHandler(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, "public/styles/style.css")
-}
-
-func imageHandler(w http.ResponseWriter, req *http.Request) {
-	// Extract the filename from the URL path
-	filename := req.URL.Path[len("/blob/"):]
-
-	// Initialize a Minio client
-	endpoint := "localhost:9000"
-	accessKeyID := "minioadmin"
-	secretAccessKey := "minioadmin"
-	useSSL := false
-	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Set the response header to indicate the content type
-	contentType := mime.TypeByExtension(filepath.Ext(filename))
-	w.Header().Set("Content-Type", contentType)
-
-	// Get the object from Minio and stream it to the response body
-	object, err := minioClient.GetObject("download", filename, minio.GetObjectOptions{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer object.Close()
-
-	if _, err := io.Copy(w, object); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 }
 
 func contactHandler(w http.ResponseWriter, req *http.Request) {
@@ -66,7 +29,7 @@ func main() {
 	http.HandleFunc("/register", users.RegisterHandler)
 	http.HandleFunc("/upload", posts.UploadHandler)
 	http.HandleFunc("/update/", posts.UpdateHandler)
-	http.HandleFunc("/blob/", imageHandler)
+	http.HandleFunc("/blob/", objects.ImageHandler)
 	http.HandleFunc("/delete/", posts.DeleteHandler)
 	http.HandleFunc("/style.css", styleSheetHandler)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
